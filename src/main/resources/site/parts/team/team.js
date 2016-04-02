75c860a1ec36c33d675472eba86f5f352ee7ca2b
Glenn Ricaud <gri@enonic.com>
2016-04-02 15:38:07 +0200

    Team profile - Team players

--- src/main/resources/site/parts/team/team.js
+++ src/main/resources/site/parts/team/team.js
@@ -1,16 +1,30 @@
 var mustacheLib = require('/lib/xp/mustache');
-var portalLib = require('/lib/xp/portal');
 var foosLib = require('/lib/foos');
+var portalLib = require('/lib/xp/portal');
 
 exports.get = function (req) {
-    var team = portalLib.getContent(); 
 
-    var players = team.data.players;
-    players
+    //Retrieves the team
+    var team = portalLib.getContent();
+
+    //Generates its stats
+    //foosLib.generateTeamStats(team);
+
+    //Retrieve the team players
+    var playersIds = foosLib.toArray(team.data.playersIds);
+    log.info(playersIds.length);
+    var players = playersIds.map(function (playerId) {
+        return foosLib.getContentByKey(playerId);
+    });
+    players.forEach(function (player) {
+        foosLib.generatePictureUrl(player);
+        foosLib.generatePageUrl(player);
+    });
 
     var view = resolve('team.html');
     var body = mustacheLib.render(view, {
-        team: team
+        team: team,
+        players: players
     });
     return {
         body: body
