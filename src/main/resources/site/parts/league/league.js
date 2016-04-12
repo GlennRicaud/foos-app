607042d63197a0a4324529ec2190536b1248e20c
Glenn Ricaud <gri@enonic.com>
2016-04-12 21:21:02 +0200

    Display refactoring

--- src/main/resources/site/parts/league/league.js
+++ src/main/resources/site/parts/league/league.js
@@ -5,8 +5,6 @@ var leagueWidgetLib = require('/lib/widgets/league/league');
 
 // Handle the GET request
 exports.get = function (req) {
-
-
     var weekGames = foosLib.getTeamGamesBetween("2016-04-11", "2016-04-18");
     var weekLeagueWidget = leagueWidgetLib.render(weekGames, 3);
 
@@ -16,16 +14,11 @@ exports.get = function (req) {
     var yearGames = foosLib.getTeamGames();
     var yearLeagueWidget = leagueWidgetLib.render(yearGames, 7);
 
-    var marchGames = foosLib.getTeamGamesBetween("2016-03-01", "2016-04-01");
-    var marchLeagueWidget = leagueWidgetLib.render(marchGames, 6);
-
-
     var view = resolve('league.html');
     var body = mustacheLib.render(view, {
         weekLeagueWidget: weekLeagueWidget,
         aprilLeagueWidget: aprilLeagueWidget,
-        yearLeagueWidget: yearLeagueWidget,
-        marchLeagueWidget: marchLeagueWidget
+        yearLeagueWidget: yearLeagueWidget
     });
     return {
         body: body
