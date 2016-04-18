028ddcbd411b877dea4ae4ff2f9d668fc237ee5e
Glenn Ricaud <gri@enonic.com>
2016-04-18 21:17:12 +0200

    League dates update Should be automatic

--- src/main/resources/site/parts/league/league.js
+++ src/main/resources/site/parts/league/league.js
@@ -5,14 +5,14 @@ var leagueWidgetLib = require('/lib/widgets/league/league');
 
 // Handle the GET request
 exports.get = function (req) {
-    var weekGames = foosLib.getTeamGamesBetween("2016-04-11", "2016-04-18");
+    var weekGames = foosLib.getTeamGamesBetween("2016-04-18", "2016-04-25");
     var weekLeagueWidget = leagueWidgetLib.render(weekGames, 3);
 
     var aprilGames = foosLib.getTeamGamesBetween("2016-04-01", "2016-05-01");
-    var aprilLeagueWidget = leagueWidgetLib.render(aprilGames, 4);
+    var aprilLeagueWidget = leagueWidgetLib.render(aprilGames, 5);
 
     var yearGames = foosLib.getTeamGames();
-    var yearLeagueWidget = leagueWidgetLib.render(yearGames, 7);
+    var yearLeagueWidget = leagueWidgetLib.render(yearGames, 9);
 
     var view = resolve('league.html');
     var body = mustacheLib.render(view, {
