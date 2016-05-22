2d558ca5069e416fefca0864dcdfdfadb7c89483
Glenn Ricaud <gri@enonic.com>
2016-05-22 22:10:24 +0200

    Quickfix news page

--- src/main/resources/site/parts/main/main.js
+++ src/main/resources/site/parts/main/main.js
@@ -1,10 +1,19 @@
 var mustacheLib = require('/lib/xp/mustache');
 var foosRetrievalLib = require('/lib/foos-retrieval');
+var foosUtilLib = require('/lib/foos-util');
 
 // Handle the GET request
 exports.get = function (req) {
 
     var releases = foosRetrievalLib.getReleases();
+
+    releases.forEach(function (release) {
+        foosUtilLib.toArray(release.data.features).forEach(function (feature) {
+            feature.gen = {
+                hasDetails: !!feature.details
+            };
+        });
+    });
 
     var view = resolve('main.html');
     var body = mustacheLib.render(view, {releases: releases});
