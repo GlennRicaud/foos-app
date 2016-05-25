603d48aa9a00962ead331546a40fe579cb7994b0
Glenn Ricaud <gri@enonic.com>
2016-05-25 21:52:44 +0200

    Stats junkie page

--- src/main/resources/site/parts/stats-junkie/stats-junkie-script.js
+++ src/main/resources/site/parts/stats-junkie/stats-junkie-script.js
@@ -1,5 +1,11 @@
 $.ajax("{{playerStatsServiceUrl}}").done(function (data) {
+
+    var index = 1;
+
     for (var playerName in data) {
+        var template = "{{{playerStatsRowTemplate}}}";
+        var row = Mustache.render(template, {displayName: playerName});
+        $("#foos-stats-junkie-table").append(row);
 
     }
     $("#foos-stats-junkie-tmp-message").hide();
