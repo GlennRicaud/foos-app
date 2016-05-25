3c2a708e6c1290c0573b2bb6092c3e9c9e591842
Glenn Ricaud <gri@enonic.com>
2016-05-25 22:09:00 +0200

    Stats junkie page

--- src/main/resources/site/parts/stats-junkie/stats-junkie-script.js
+++ src/main/resources/site/parts/stats-junkie/stats-junkie-script.js
@@ -3,10 +3,15 @@ $.ajax("{{playerStatsServiceUrl}}").done(function (data) {
     var index = 1;
 
     for (var playerName in data) {
-        var template = "{{{playerStatsRowTemplate}}}";
-        var row = Mustache.render(template, {displayName: playerName});
+        var template = '{{{playerStatsRowTemplate}}}';
+        var row = Mustache.render(template, {
+            even: (index % 2) == 0,
+            rank: index,
+            displayName: playerName,
+            value: data[playerName].nbGames.total
+        });
         $("#foos-stats-junkie-table").append(row);
-
+        index++;
     }
     $("#foos-stats-junkie-tmp-message").hide();
     //$("#foos-stats-junkie-table").html("");
