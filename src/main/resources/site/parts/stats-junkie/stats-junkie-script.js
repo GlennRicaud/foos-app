1660ded4bad7a564ff241608befa236eb17330e7
Glenn Ricaud <gri@enonic.com>
2016-05-25 22:19:55 +0200

    Stats junkie page

--- src/main/resources/site/parts/stats-junkie/stats-junkie-script.js
+++ src/main/resources/site/parts/stats-junkie/stats-junkie-script.js
@@ -11,8 +11,16 @@ $.ajax("{{playerStatsServiceUrl}}").done(function (data) {
             value: data[playerName].nbGames.total
         });
         $("#foos-stats-junkie-table").append(row);
+
+        if (index == 1) {
+            for (var statName in data[playerName]) {
+                $("#foos-stats-junkie-select").append("<option>" + data[playerName][statName].name + "</option>");
+            }
+        }
         index++;
     }
+
+
     $("#foos-stats-junkie-tmp-message").hide();
     //$("#foos-stats-junkie-table").html("");
 });
