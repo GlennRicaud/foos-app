ef824d2505e3bca826755ed6ac8cec9ddfeae171
Glenn Ricaud <gri@enonic.com>
2016-05-25 20:10:53 +0200

    Stats junkie page

--- src/main/resources/site/parts/stats-junkie/stats-junkie.js
+++ src/main/resources/site/parts/stats-junkie/stats-junkie.js
@@ -5,11 +5,13 @@ var foosRetrievalLib = require('/lib/foos-retrieval');
 // Handle the GET request
 exports.get = function (req) {
     var view = resolve('stats-junkie.html');
+    var scriptView = resolve('stats-junkie-script.js');
     var body = mustacheLib.render(view, {});
     return {
         body: body,
         pageContributions: {
-            headEnd: '<script src="' + portalLib.assetUrl({path: "js/jquery-2.2.4.min.js"}) + '""/></script>'
+            headEnd: '<script src="' + portalLib.assetUrl({path: "js/jquery-2.2.4.min.js"}) + '""/></script>',
+            bodyEnd: '<script>' + mustacheLib.render(scriptView, {}) + '</script>'
         }
     }
 };
