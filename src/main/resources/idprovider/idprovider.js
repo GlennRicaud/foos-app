217a8d13a09f5f47c689c95f99506a869a9ec090
Glenn Ricaud <gri@enonic.com>
2016-06-04 01:34:21 +0200

    AuthFilter

--- src/main/resources/idprovider/idprovider.js
+++ src/main/resources/idprovider/idprovider.js
@@ -1,6 +1,8 @@
 var mustacheLib = require('/lib/xp/mustache');
 var portalLib = require('/lib/xp/portal');
 var authLib = require('/lib/xp/auth');
+var contextLib = require('/lib/xp/context');
+var foosUtilLib = require('/lib/foos-util');
 
 exports.handle403 = function (req) {
     var body = generateLoginPage();
@@ -21,6 +23,52 @@ exports.login = function (req) {
     };
 };
 
+exports.authFilter = function (req) {
+    var idProviderConfig = authLib.getIdProviderConfig();
+
+    var allowedIps = foosUtilLib.toArray(idProviderConfig.allowedIps);
+    if (allowedIps.indexOf(req.remoteAddress) != -1) {
+
+        initLocalUser();
+        var result = authLib.login({
+            user: "local-user",
+            password: authLib.getIdProviderConfig().password, //TODO Temp fix
+            userStore: authLib.getUserStore().key
+        });
+    }
+};
+
+function initLocalUser() {
+    contextLib.run({
+        user: {
+            login: 'gri',
+            userStore: 'system'
+        }
+    }, doInitLocalUser);
+}
+
+function doInitLocalUser() {
+    var localUserKey = "user:" + authLib.getUserStore().key + ":local-user";
+    var localUser = authLib.getPrincipal(localUserKey);
+    if (!localUser) {
+        localUser = authLib.createUser({
+            userStore: authLib.getUserStore().key,
+            name: 'local-user',
+            displayName: 'Local User',
+            email: 'local-user@foos.es'
+        });
+
+        //TODO Temp fix
+        authLib.changePassword({
+            userKey: localUser.key,
+            password: authLib.getIdProviderConfig().password
+        });
+
+        authLib.addMembers('role:system.admin.login', [localUserKey]);
+        authLib.addMembers('role:cms.cm.app', [localUserKey]);
+    }
+}
+
 exports.logout = function (req) {
     authLib.logout();
 
@@ -31,7 +79,6 @@ exports.logout = function (req) {
 
 function generateLoginPage(redirectUrl) {
     var userStoreKey = authLib.getUserStore().key;
-    var idProviderConfig = authLib.getIdProviderConfig();
 
     var jQueryUrl = portalLib.assetUrl({path: "js/jquery-2.2.4.min.js"});
     var loginScriptUrl = portalLib.assetUrl({path: "js/login.js"});
