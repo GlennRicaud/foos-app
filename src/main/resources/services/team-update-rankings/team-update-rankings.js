9786c420843331c9b1df74f5f818685048739473
Alex Rodriguez <aro@enonic.com>
2016-10-18 09:01:33 +0200

    Calculate and show team ranking points in game details pages

--- src/main/resources/services/team-update-rankings/team-update-rankings.js
+++ src/main/resources/services/team-update-rankings/team-update-rankings.js
@@ -2,7 +2,7 @@ var foosRatingLib = require('/lib/foos-rating');
 
 // Recalculate teams position (ranking) based on rating points already assigned for the team
 exports.get = function () {
-    foosRatingLib.calculateGameTeamRatings();
+    foosRatingLib.updateTeamRankings();
 
     return {
         contentType: 'application/json',
