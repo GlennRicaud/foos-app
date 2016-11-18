d62301a5d67dbd65267cc600f1c97c593efa71f7
Glenn Ricaud <gri@enonic.com>
2016-11-18 23:58:35 +0100

    Logout menu item

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,12 +1,6 @@
 $('#foos-menu-item-user').click(function () {
     var userNameBoundingClientRect = this.getBoundingClientRect();
     $('#foos-menu-item-logout').toggleClass('hidden');
-    $('#foos-menu-item-logout').css('position', 'absolute');
     $('#foos-menu-item-logout').css('right', 0);
     $('#foos-menu-item-logout').css('top', userNameBoundingClientRect.bottom);
-
-});
-
-$('#foos-menu-item-logout').mouseleave(function () {
-    $('#foos-menu-item-logout').addClass('hidden');
 });
