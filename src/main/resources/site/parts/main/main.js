4d44f9eba6fa2cd825323d056a5fb6aa9cb71651
Alex Rodriguez <aro@enonic.com>
2016-11-23 08:37:45 +0100

    Temporary main page cleanup

--- src/main/resources/site/parts/main/main.js
+++ src/main/resources/site/parts/main/main.js
@@ -1,22 +1,12 @@
 var mustacheLib = require('/lib/xp/mustache');
-var foosRetrievalLib = require('/lib/foos-retrieval');
-var foosUtilLib = require('/lib/foos-util');
+var portalLib = require('/lib/xp/portal');
 
 // Handle the GET request
 exports.get = function (req) {
-
-    var releases = foosRetrievalLib.getReleases();
-
-    releases.forEach(function (release) {
-        foosUtilLib.toArray(release.data.features).forEach(function (feature) {
-            feature.gen = {
-                hasDetails: !!feature.details
-            };
-        });
-    });
-
     var view = resolve('main.html');
-    var body = mustacheLib.render(view, {releases: releases});
+    var logoImg = portalLib.assetUrl({path: "img/logo.svg"})
+
+    var body = mustacheLib.render(view, {logoImg: logoImg});
     return {
         body: body
     }
