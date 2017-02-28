88f7b41c3b47c9d6bf7e537d7d4ed2a5e63ca798
Alex Rodriguez <aro@enonic.com>
2017-02-28 21:19:14 +0100

    Game chart improvements

--- src/main/resources/assets/js/game.js
+++ src/main/resources/assets/js/game.js
@@ -1,7 +1,21 @@
 $(function () {
     var ctx = $('#gameChart');
 
-    var dataWinner = [], dataLoser = [], goal, winnerScore = 0, loserScore = 0;
+    var originalDraw = Chart.controllers.line.prototype.draw;
+    Chart.controllers.line.prototype.draw = function (ease) {
+        originalDraw.call(this, ease);
+        var scale = this.chart.scales['x-axis-0'];
+        var left = scale.getPixelForTick(tickIndex, false);
+        // draw vertical line
+        this.chart.chart.ctx.beginPath();
+        this.chart.chart.ctx.strokeStyle = '#888888';
+        this.chart.chart.ctx.lineWidth = 1.5;
+        this.chart.chart.ctx.moveTo(left, 32);
+        this.chart.chart.ctx.lineTo(left, 276);
+        this.chart.chart.ctx.stroke();
+    };
+
+    var dataWinner = [], dataLoser = [], goal, winnerScore = 0, loserScore = 0, midTime = 0, tickIndex=0;
     dataWinner.push({x: 0, y: 0});
     dataLoser.push({x: 0, y: 0});
     for (var i = 0, l = GOALS.length; i < l; i++) {
@@ -20,6 +34,9 @@ $(function () {
             x: goal.time,
             y: loserScore
         });
+        if (midTime === 0 && ( winnerScore === 5 || loserScore === 5)) {
+            midTime = goal.time;
+        }
     }
 
     var ds1 = {
@@ -65,7 +82,24 @@ $(function () {
             scales: {
                 xAxes: [{
                     type: 'linear',
-                    position: 'bottom'
+                    position: 'bottom',
+                    ticks: {
+                        min: 0,
+                        callback: function (label, index, labels) {
+                            return label == midTime ? '' : formatSeconds(parseInt(label, 10));
+                        }
+                    },
+                    afterBuildTicks: function (chartTicks) {
+                        if (chartTicks.ticks && chartTicks.ticks.length > 2) {
+                            for (var i = 1; i < chartTicks.ticks.length; i++) {
+                                if (chartTicks.ticks[i - 1] <= midTime && chartTicks.ticks[i] >= midTime) {
+                                    chartTicks.ticks.splice(i, 0, midTime);
+                                    tickIndex = i;
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }],
                 yAxes: [{
                     ticks: {
@@ -78,3 +112,16 @@ $(function () {
     });
 
 });
+
+var formatSeconds = function (s) {
+    if (s <= 0) {
+        return '';
+    }
+    var date = new Date(null);
+    date.setSeconds(s);
+    if (s < 3600) {
+        return date.toISOString().substr(14, 5)
+    } else {
+        return date.toISOString().substr(11, 8);
+    }
+};
